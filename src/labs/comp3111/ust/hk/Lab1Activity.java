86de3d7232e70b5be3365018b7f21b23116b3391
Michael Frost <frostyfireball@hotmail.com>
2018-02-11 21:02:15 +0800

    Implemented the sum of 10 integers

--- src/labs/comp3111/ust/hk/Lab1Activity.java
+++ src/labs/comp3111/ust/hk/Lab1Activity.java
@@ -3,8 +3,17 @@ package labs.comp3111.ust.hk;
 public class Lab1Activity {
 
 	public static void main(String[] args) {
+		//Array to sum
+		int[] arr = {1,2,3,4,5,6,7,8,9,10};
+		int sum = 0;
 		
-
+		//Iterate through the array to calculate the summation
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+		}
+		
+		//Print out the formatted result
+		System.out.println(String.format("Sum of numbers is %d", sum));
 	}
 
 }
